ced9fd815072773715ca1ac139cf1d0abaf4033e
HigorC <higor_oliveira@outlook.com>
2020-06-06 20:51:58 -0300

    Throttle and debounce fixes, general count works

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,14 +1,67 @@
 <template>
-  <div class="box" @click="clickOnBox()">{{countClick}}</div>
+  <div class="box">
+    <div class="general-count">
+      <h2>
+        Você clicou
+        <strong>{{countClick}}</strong>
+        vezes
+      </h2>
+    </div>
+    <div class="throttle-area">
+      <h2>Mas a função Throttle só deixou a cor mudar {{throttleInfos.countChange}} vezes</h2>
+    </div>
+    <div class="debounce-area">
+      <h2>Mas a função Debounce só deixou a cor mudar {{debounceInfos.countChange}} vezes</h2>
+    </div>
+  </div>
 </template>
-
 <script>
-import { getRandonInteger, throttle } from "../util";
+import { getRandonInteger, throttle, debounce } from "../util";
 export default {
   name: "Box",
+  mounted: function() {
+    const that = this;
+    const boxElement = document.querySelector(".box");
+    if (boxElement) {
+      boxElement.addEventListener("click", () => this.countClick++);
+      boxElement.addEventListener(
+        "click",
+        throttle(
+          () => this.changeBoxColor(".throttle-area", that.throttleInfos),
+          1500
+        )
+      );
+      boxElement.addEventListener(
+        "click",
+        debounce(
+          () => this.changeBoxColor(".debounce-area", that.debounceInfos),
+          1500
+        )
+      );
+      boxElement.addEventListener(
+        "click",
+        debounce(() => that.resetCounters(), 3000)
+      );
+    }
+  },
   data: () => ({
     countClick: 0,
+    throttleInfos: {
+      countChange: 0,
+      indexActualRandonColor: -1
+    },
+    debounceInfos: {
+      countChange: 0,
+      indexActualRandonColor: -1
+    },
     colors: [
+      "#1e0492",
+      "#550fcb",
+      "#e07df3",
+      "#6a6582",
+      "#c4b6cc",
+      "#ADFF02",
+      "#34343a",
       "#01BEFE",
       "#FFDD00",
       "#FF7D00",
@@ -18,29 +71,107 @@ export default {
       "#8F00FF"
     ]
   }),
+  computed: {},
   methods: {
+    resetCounters: function() {
+      const interval = setInterval(()=> {
+        if (this.countClick > 0) {
+          this.countClick--;
+        }
+        if (this.throttleInfos.countChange > 0) {
+          this.throttleInfos.countChange--;
+        }
+        if (this.debounceInfos.countChange > 0) {
+          this.debounceInfos.countChange--;
+        }
+
+        if (this.countClick + this.throttleInfos.countChange + this.debounceInfos.countChange === 0)  {
+          clearInterval(interval);
+        }
+      }, 20);
+    },
     clickOnBox: function() {
       this.countClick++;
-      throttle(this.changeBoxColor, 1000);
+      const that = this;
+      throttle(
+        () => this.changeBoxColor(".throttle-area", that.throttleInfos),
+        1000
+      );
     },
-    incrementCountClick: function() {
-      this.countClick++;
-    },
-    changeBoxColor: function() {
-      const randonColor = this.colors[getRandonInteger(this.colors.length)];
-      const boxElement = document.querySelector(".box");
+    changeBoxColor: function(selector, objInfo) {
+      objInfo.indexActualRandonColor = getRandonInteger(
+        this.colors.length,
+        objInfo.indexActualRandonColor
+      );
+      const randonColor = this.colors[objInfo.indexActualRandonColor];
+      const boxElement = document.querySelector(selector);
       if (boxElement) {
-        boxElement.style.background = randonColor;
+        setTimeout(() => {
+          boxElement.style.background = randonColor;
+        }, 100);
       }
+      objInfo.countChange++;
     }
   }
 };
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+@import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+
 .box {
-  transition: all 0.7s;
   height: 100%;
   width: 100%;
+  display: flex;
+  align-items: center;
+  h2 {
+    @extend .noselect;
+    font-family: "Press Start 2P", cursive;
+    color: #ccc;
+    text-shadow: 3px 3px 2px #000000;
+  }
+
+  .general-count {
+    background-color: #000;
+    padding: 15px 5px;
+    top: 7%;
+    width: 550px;
+    left: 50%;
+    // margin-right: -50%;
+    transform: translate(-50%, -50%);
+    h2 {
+      margin: 0;
+    }
+    // margin-top: 5%;
+    position: absolute;
+  }
+
+  .throttle-area,
+  .debounce-area {
+    background-color: #34343a;
+    transition: all 0.7s;
+    height: 100%;
+    width: 50%;
+    display: flex;
+    align-items: center;
+  }
+
+  .throttle-area {
+    float: left;
+  }
+
+  .debounce-area {
+    float: right;
+  }
+}
+
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
 }
 </style>
+ 
--- src/util.js
+++ src/util.js
@@ -1,21 +1,44 @@
+import { setTimeout } from "core-js"
+
 /**
  * Returns a randon intenger in a limit range
  * @param { Number } limit 
  */
-const getRandonInteger = (limit) => {
-    return Math.floor(Math.random() * limit) + 1
+const getRandonInteger = (limit, lastGenerated) => {
+    let generatedInt = Math.floor(Math.random() * limit) + 1
+
+    while (generatedInt === lastGenerated) {
+        generatedInt = Math.floor(Math.random() * limit) + 1
+    }
+
+    return generatedInt
 }
 
-let canCallThrottle = false
-
 function throttle(callback, delay) {
-    if (!canCallThrottle) {
-        callback()
-        canCallThrottle = true
-        canCallThrottle = setTimeout(() => {
-            canCallThrottle = false
-        }, delay);
+    let canCallThrottle
+
+    return function () {
+        if (!canCallThrottle) {
+            callback()
+            canCallThrottle = true
+            canCallThrottle = setTimeout(() => {
+                canCallThrottle = false
+            }, delay);
+        }
     }
 }
 
-export { getRandonInteger, throttle }
+
+const debounce = (fn, time) => {
+    let timeout;
+
+    return function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(
+            () => fn.apply(this, arguments),
+            time
+        );
+    }
+}
+
+export { getRandonInteger, throttle, debounce }
